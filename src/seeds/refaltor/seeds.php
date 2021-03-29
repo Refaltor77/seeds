5f7ec0c15e0bc1215b548c492bf63f54d0cba8c4
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-03-29 16:38:28 +0200

    Update seeds.php

--- src/seeds/refaltor/seeds.php
+++ src/seeds/refaltor/seeds.php
@@ -2,7 +2,19 @@
 
 namespace seeds\refaltor;
 
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use seeds\refaltor\Blocks\Beetroot;
+use seeds\refaltor\Blocks\Carrot;
+use seeds\refaltor\Blocks\Melon;
+use seeds\refaltor\Blocks\Nether_wart;
+use seeds\refaltor\Blocks\Potato;
+use seeds\refaltor\Blocks\Pumpkin;
+use seeds\refaltor\Blocks\Sugarcane;
+use seeds\refaltor\Blocks\TallGrass;
+use seeds\refaltor\Blocks\Wheat;
 use seeds\refaltor\events\dropsListener;
 
 class seeds extends PluginBase
@@ -16,6 +28,15 @@ class seeds extends PluginBase
     public function onEnable(){
         $this->saveDefaultConfig();
         self::$instance = $this;
-        self::getServer()->getPluginManager()->registerEvents(new dropsListener(), $this);
+        BlockFactory::registerBlock(new Wheat(), true);
+        BlockFactory::registerBlock(new Potato(), true);
+        BlockFactory::registerBlock(new Carrot(), true);
+        BlockFactory::registerBlock(new Nether_wart(), true);
+        BlockFactory::registerBlock(new Melon(), true);
+        BlockFactory::registerBlock(new Sugarcane(), true);
+        BlockFactory::registerBlock(new Pumpkin(), true);
+        BlockFactory::registerBlock(new Beetroot(), true);
+        BlockFactory::registerBlock(new TallGrass(), true);
+        Server::getInstance()->getPluginManager()->registerEvents(new dropsListener(), $this);
     }
 }
